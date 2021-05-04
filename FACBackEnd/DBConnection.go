7ee6163297b79f5e624c8f049675d120c509076d
Jimmy Guo <jimmyg8@cs.washington.edu>
2021-05-03 18:06:31 -0700

    Updated DbConnection.go to have adding functionality

--- FACBackEnd/DBConnection.go
+++ FACBackEnd/DBConnection.go
@@ -1,39 +1,64 @@
 package main
 
-
 import (
-	"context"
-	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+    "fmt"
 	"log"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"strconv"
 )
 
+type Location struct {
+	Lat float64
+	Lng float64
+	IsGarbage bool
+	IsRecycling bool
+	IsCompost bool
+}
+
+func AddLocation(svc *dynamodb.DynamoDB, err error, latCoord float64, lngCoord float64, garb bool, recycle bool, compost bool) {
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials
+	// and region from the shared configuration file ~/.aws/config.
+	
+	item := Location{
+		Lat:   latCoord,
+		Lng: lngCoord,
+		IsGarbage:   garb,
+		IsRecycling: recycle,
+		IsCompost: compost,
+	}
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+    	log.Fatalf("Got error marshalling new location item: %s", err)
+	}
+	tableName := "Locations"
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+	
+	_, err = svc.PutItem(input)
+	if err != nil {
+		log.Fatalf("Got error calling PutItem: %s", err)
+	}
+	
+	addedLat := strconv.FormatFloat(item.Lat, 'E', -1, 64)
+	addedLng := strconv.FormatFloat(item.Lat, 'E', -1, 64)
+	
+	fmt.Println("Successfully added '" + addedLat + ", " + addedLng + " to table " + tableName)
+}
+
 func main() {
-    // Using the SDK's default configuration, loading additional config
-    // and credentials values from the environment variables, shared
-    // credentials, and shared configuration files
-    cfg, err := config.LoadDefaultConfig(context.TODO(),
-   		config.WithRegion("us-west-2"),
-   	)
-    if err != nil {
-        log.Fatalf("unable to load SDK config, %v", err)
-    }
+	sess, err := session.NewSession(&aws.Config{
+        Region: aws.String("us-west-2")},
+    )
 
-    // Using the Config value, create the DynamoDB client
-    svc := dynamodb.NewFromConfig(cfg)
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+	AddLocation(svc, err, 47.5638179, -122.1675207, false, true, true)
 
-    // Build the request with its input parameters
-    resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
-        Limit: aws.Int32(5),
-    })
-    if err != nil {
-        log.Fatalf("failed to list tables, %v", err)
-    }
-
-    fmt.Println("Tables:")
-    for _, tableName := range resp.TableNames {
-        fmt.Println(tableName)
-    }
 }
