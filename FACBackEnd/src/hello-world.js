fb1835de7e50bb39682983889315db99fba141c6
kumararoosh <24703104+kumararoosh@users.noreply.github.com>
2021-05-09 14:09:39 -0700

    Added more dummy data

--- FACBackEnd/src/hello-world.js
+++ FACBackEnd/src/hello-world.js
@@ -4,20 +4,78 @@ const app = express()
 const port = 3000
 
 const dummyData = {
-  type: 'Feature',
-  geometry: {
-    type: 'Point',
-    coordinates: [125.6, 10.1]
-  },
-  properties: {
-    IsTrash: true,
-    IsRecycling: true,
-    IsCompost: true
-  }
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "tomato",
+        "marker-size": "medium",
+        "marker-symbol": "square"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.30759382247925,
+          47.65881179780758
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "blue",
+        "marker-size": "medium",
+        "marker-symbol": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.30946063995361,
+          47.65437463432688
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "yellow",
+        "marker-size": "medium",
+        "marker-symbol": "triangle"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.30370998382567,
+          47.65544421310926
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "aqua",
+        "marker-size": "medium",
+        "marker-symbol": "circle"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.30332374572754,
+          47.6584071209998
+        ]
+      }
+    }
+  ]
 }
 
 app.get('/locations', (req, res) => {
   res.json(dummyData)
+})
+
+app.post('/update', (req, res) => {
+  console.log(req.query)
+  res.send("recieved!")
 })
 
 app.listen(port, () => console.log(`Hello world app is listening on port ${port}!`))
