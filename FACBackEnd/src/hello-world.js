5437da27eba26c5dd3954d9dc401f27cf13fb72c
kumararoosh <24703104+kumararoosh@users.noreply.github.com>
2021-05-09 13:54:30 -0700

    Added support for sending dummy data in json

--- FACBackEnd/src/hello-world.js
+++ FACBackEnd/src/hello-world.js
@@ -3,8 +3,21 @@ const app = express()
 
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+const dummyData = {
+  type: 'Feature',
+  geometry: {
+    type: 'Point',
+    coordinates: [125.6, 10.1]
+  },
+  properties: {
+    IsTrash: true,
+    IsRecycling: true,
+    IsCompost: true
+  }
+}
+
+app.get('/locations', (req, res) => {
+  res.json(dummyData)
 })
 
 app.listen(port, () => console.log(`Hello world app is listening on port ${port}!`))
