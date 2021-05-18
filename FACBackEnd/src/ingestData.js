129e084721b974d80b2a46fdb49816d92aaa8672
kumararoosh <24703104+kumararoosh@users.noreply.github.com>
2021-05-18 14:34:53 -0700

    Added comments and fixed functions in ingestData.js

--- FACBackEnd/src/ingestData.js
+++ FACBackEnd/src/ingestData.js
@@ -2,17 +2,24 @@ const csv = require('csv-parser')
 const fs = require('fs')
 const putItem = require('./putItem')
 
+/**
+ * Takes a path to a local .csv file that contains data to be pushed to an AWS DynamoDB instance
+ * The .csv file must have the headers 'LATITUDE', 'LONGITUDE' as numbers and the headers
+ * 'isGarbage', 'isCompost', 'isRecycling' as boolean values
+ * 
+ * The success/failure of operations are logged on the console.
+ * @param {string} relativePath 
+ * 
+ */
 function readFromCSV (relativePath) {
-  fs.createReadStream('./sampledata/seattlecanlocations.csv')
+  fs.createReadStream(relativePath)
     .pipe(csv())
     .on('data', (row) => {
-      // obj = JSON.parse(row)
       putItem.putItem('' + row.LATITUDE, '' + row.LONGITUDE, Boolean(row.isGarbage), Boolean(row.isCompost), Boolean(row.isRecycling))
         .then((result, error) => {
           if (error) console.log(error)
           else console.log(result)
         })
-        // console.log(Boolean(row.isGarbage))
     })
     .on('end', () => {
       console.log('finished')
