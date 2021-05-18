c9ac5ffe4e456c0e939cf5e97efe6beb15a4e8df
kumararoosh <24703104+kumararoosh@users.noreply.github.com>
2021-05-18 14:42:27 -0700

    Fixed linting issues in ingestData.js

--- FACBackEnd/src/ingestData.js
+++ FACBackEnd/src/ingestData.js
@@ -6,10 +6,10 @@ const putItem = require('./putItem')
  * Takes a path to a local .csv file that contains data to be pushed to an AWS DynamoDB instance
  * The .csv file must have the headers 'LATITUDE', 'LONGITUDE' as numbers and the headers
  * 'isGarbage', 'isCompost', 'isRecycling' as boolean values
- * 
+ *
  * The success/failure of operations are logged on the console.
- * @param {string} relativePath 
- * 
+ * @param {string} relativePath
+ *
  */
 function readFromCSV (relativePath) {
   fs.createReadStream(relativePath)
